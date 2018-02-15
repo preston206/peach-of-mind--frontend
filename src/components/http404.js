9ea2b5c0f85fecf112409d68ccc9f5bbaf731925
preston <broberts9@gmail.com>
2018-02-15 15:18:54 -0800

    adding navbar component to custom 404 page

--- src/components/http404.js
+++ src/components/http404.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Nav from './nav';
 
 const Http404 = () => {
     return (
         <div>
-            <p>404: </p>
-            <p>we are unable to find that resource.</p>
+            <Nav />
+            <div className="ui center aligned container pad-10em">
+                <h1>404: </h1>
+                <h2>we are unable to find that resource.</h2>
+            </div>
         </div>
     )
 };
